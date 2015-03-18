2c93ea54472e1654070a7ed76f807fd1dc65274b
shadowbq <shadowbq@gmail.com>
2015-03-18 12:41:36 -0400

    remove pry
    remove pry from master branch.

--- lib/ruby-fifo.rb
+++ lib/ruby-fifo.rb
@@ -2,7 +2,6 @@ libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
 require 'forwardable'
-require 'pry'
 
 if RUBY_PLATFORM =~ /mswin/
   require 'web32/pipe'
